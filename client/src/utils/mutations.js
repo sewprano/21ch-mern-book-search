cbd32f68177c9e8f50a4e22b967ed490147b02ac
Deborah Wade <sopranosamurai@yahoo.com>
2023-01-09 18:23:26 -0800

    added front-end mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import {gql} from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+  }
+}
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+  }
+}
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($newBook: NewBook!) {
+  saveBook(newBook: $newBook) {
+    _id
+    email
+    username
+    savedBooks {
+      authors
+      bookId
+      descrition
+      image
+      link
+      title
+    }
+  }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+  removeBook(bookId: $bookId) {
+    _id
+    email
+    username
+    savedBooks {
+      authors
+      bookId
+      descrition
+      image
+      link
+      title
+    }
+  }
+}
+`;
