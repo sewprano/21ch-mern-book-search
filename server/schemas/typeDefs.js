d9f398c01bb2744f3270696063a9d70f37c7b01f
Deborah Wade <sopranosamurai@yahoo.com>
2023-01-09 17:04:10 -0800

    finished typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID 
+        username: String
+        email: String
+        savedBooks:[Book]
+    }
+    type Book {
+        authors: [String]
+        descrition: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input NewBook {
+        authors: [String]
+        descrition: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: User
+    } 
+
+    type Mutation {
+        login(email: String!, password:String!): Auth 
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(newBook: NewBook!): User
+        removeBook(bookId: String!): User
+    }
+    
+`;
+
+module.export = typeDefs;
